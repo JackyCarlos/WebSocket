80c52d6e4ff38b766dffa7b012e24f9f9fbdbd12
Robert Eikmanns <robert.eikmanns@posteo.net>
2023-07-13 22:43:11 +0200

    add shiftLeft and generateWords helper functions

--- sha1/sha1.h
+++ sha1/sha1.h
@@ -5,7 +5,8 @@
 typedef struct {
 	uint32_t 	intermediateHash[HASHSIZE / 4];
 	uint8_t 	messageBlock[64];			// array holding the next message block
-
+	
+	uint32_t 	index;					// index for position in message Block
 } sha1Context;
 
 void sha1Init(sha1Context *);
@@ -13,3 +14,6 @@ void sha1ProcessBlock(sha1Context *);
 void sha1PadMessage(sha1Context *);
 void sha1Input(uint8_t *, uint32_t messageLength, sha1Context *);
 void sha1Output(sha1Context *);
+
+uint32_t shiftLeft(uint32_t);
+uint32_t generateWords(uint32_t *);
