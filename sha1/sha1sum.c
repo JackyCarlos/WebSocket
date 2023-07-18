75608f87861365394d9a6709220033a0d8904e7a
Robert Eikmanns <Robert.Eikmanns@posteo.net>
2023-07-18 22:07:14 +0200

    implement hexdigest function, add missing headers, some testing

--- sha1/sha1sum.c
+++ sha1/sha1sum.c
@@ -1,19 +1,37 @@
 #include "sha1.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+char *hexdigest(sha1Context *);
 
 int main(int argc, char *argv[]) 
 {
 	sha1Context context;
 	sha1Init(&context);
 
-	char *test = "ABC";
+	char *test = "\0";
+	unsigned char messageDigest[20];	
 
-	sha1Input((uint8_t *) test, (uint64_t) 3, &context);
-
-	for (int i = 0; i < 5; i++)
-		printf("%08x", context.intermediateHash[i]);
-	printf("\n");
+	sha1Input((uint8_t *) test, (uint64_t) 0, &context);
+	
+	printf("%s\n", hexdigest(&context));
 
 	return 0;
+}
+
+char *hexdigest(sha1Context *context)
+{
+	int i;
+	char *hexdigest = (char *) malloc(HASHSIZE + 1000);
+	char temp[9];
+
+	for (i = 0; i < 5; i++) {
+		sprintf(temp, "%08x", context->intermediateHash[i]);
+		strcat(hexdigest, temp);
+	}
+
+	return hexdigest;
 }
