49c7d41e844c410e47267013f6301144a8a60066
Robert Eikmanns <r.eikmanns@alexander-buerkle.de>
2023-07-18 18:34:52 +0200

    add testing stuff

--- sha1/sha1sum.c
+++ sha1/sha1sum.c
@@ -1,6 +1,19 @@
 #include "sha1.h"
+#include <stdint.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) 
 {
+	sha1Context context;
+	sha1Init(&context);
+
+	char *test = "ABC";
+
+	sha1Input((uint8_t *) test, (uint64_t) 3, &context);
+
+	for (int i = 0; i < 5; i++)
+		printf("%08x", context.intermediateHash[i]);
+	printf("\n");
+
 	return 0;
 }
