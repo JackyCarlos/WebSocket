a907bb828fddb78ef93b7bb7ad35bcb38f10daea
Robert Eikmanns <r.eikmanns@alexander-buerkle.de>
2023-07-19 14:23:37 +0200

    implement sha1sum function with the help of the coded sha1 API

--- sha1/sha1sum.c
+++ sha1/sha1sum.c
@@ -11,21 +11,50 @@ int main(int argc, char *argv[])
 {
 	sha1Context context;
 	sha1Init(&context);
-
-	char *test = "\0";
-	unsigned char messageDigest[20];	
-
-	sha1Input((uint8_t *) test, (uint64_t) 0, &context);
 	
-	printf("%s\n", hexdigest(&context));
+	if (argc == 1) {
+		int i, j = 0;
+		char input[1000];
+		while ((i = getchar()) != EOF && j < 10000)
+			input[j++] = i;
 
+		sha1Input((uint8_t *) input, j, &context);
+		printf("%s\n", hexdigest(&context));
+
+		return 0;
+	}
+
+	while (--argc) {
+		FILE *f;
+
+		if((f = fopen(*++argv, "r")) == NULL) {
+			printf("sha1sum: can't open %s. No such file\n", *argv);
+			continue;
+		}
+
+		fseek(f, 0L, SEEK_END);
+		long size = ftell(f);
+		fseek(f, 0L, SEEK_SET);
+		
+		int i, j = 0;
+		char input[size];
+		
+		while ((i = getc(f)) != EOF)
+			input[j++] = i;
+
+		sha1Input((uint8_t *) input, size, &context);
+		printf("%s  %s\n", hexdigest(&context), *argv);
+		fclose(f);
+
+		sha1Init(&context);
+	}
 	return 0;
 }
 
 char *hexdigest(sha1Context *context)
 {
 	int i;
-	char *hexdigest = (char *) malloc(HASHSIZE + 1000);
+	char *hexdigest = (char *) malloc(2 * HASHSIZE + 1);
 	char temp[9];
 
 	for (i = 0; i < 5; i++) {
@@ -35,3 +64,28 @@ char *hexdigest(sha1Context *context)
 
 	return hexdigest;
 }
+
+/*
+	sha1Context context;
+	sha1Init(&context);
+
+	char *test = "   ";
+	unsigned char messageDigest[20];	
+
+	sha1Input((uint8_t *) test, 3, &context);
+	
+	//printf("%s\n", hexdigest(&context));
+
+	sha1Output(messageDigest, &context);
+	char hexdigest[41];
+	char temp[3];
+
+	for (int i = 0; i < HASHSIZE; i++) {
+		sprintf(temp, "%02x", messageDigest[i]);
+		strcat(hexdigest, temp);
+	}
+
+	printf("%s\n", hexdigest);
+
+	return 0;
+*/
