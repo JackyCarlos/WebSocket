632202f7f5b498bd9c148b88d0ee990cf37b658d
Robert Eikmanns <Robert.Eikmanns@posteo.net>
2024-02-25 18:58:24 +0100

    add dynamic memory allocation for headers

--- http.c
+++ http.c
@@ -1,34 +1,44 @@
 #include "ws.h"
 
 void 
-parse_http_request(char *request, char *method, HTTP_header *request_headers, int *count) 
-{
+parse_http_request(char *request, char *method, http_header_t **request_headers, int *count) 
+{	
 	char *request_line, *raw_header, *header;
-	int pos;
-	char *raw_headers[20];
+	int header_count, max_header;
+	char **raw_headers;
+
+	header_count = 0;
+	max_header = 20;
+	raw_headers = malloc(max_header * sizeof(char *));
 	
 	// get rid of the http payload
 	char *header_end = strstr(request, "\r\n\r\n");
 	*header_end = '\0';
 
-	pos = -1;
-	raw_header = strtok(request, "\r\n");
+	request_line = strtok(request, "\r\n");
+	raw_header = strtok(NULL, "\r\n");
+
 	while (raw_header != NULL) {
-		if (pos == -1) {
-			request_line = raw_header;
-			pos++;	
-		} else 
-			raw_headers[pos++] = raw_header;
+		raw_headers[header_count++] = raw_header;
+
+		if (header_count == max_header) {
+			max_header += 10;
+			raw_headers = realloc(raw_headers, max_header * sizeof(char *));
+		} 
 		raw_header = strtok(NULL, "\r\n");
 	}
-	*count = pos;
+	*count = header_count;
 
 	// extract http method
 	strcpy(method, strtok(request_line, " "));
 
-	for (int i = 0; i < pos; i++) {
+	*request_headers = malloc(header_count * sizeof(http_header_t));
+
+	for (int i = 0; i < header_count; ++i) {
 		raw_header = raw_headers[i];
-		request_headers[i].header = strtok(raw_header, ": ");
-		request_headers[i].value = strtok(NULL, "");
+		(*request_headers)[i].header = strtok(raw_header, ": ");
+		(*request_headers)[i].value = strtok(NULL, "");
 	}
+
+	free(raw_headers);
 }
