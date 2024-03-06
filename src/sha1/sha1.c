d088a698f2e02948b570463bfcb11019a7b158aa
Robert Eikmanns <Robert.Eikmanns@posteo.net>
2024-03-07 00:32:29 +0100

    beautify

--- src/sha1/sha1.c
+++ src/sha1/sha1.c
@@ -13,8 +13,7 @@ static const uint32_t K3 = 0x8f1bbcdc;
 static const uint32_t K4 = 0xca62c1d6;
 
 void 
-sha1_init(sha1_context_t *context) 
-{
+sha1_init(sha1_context_t *context) {
 	context->intermediate_hash[0] = 0x67452301;
 	context->intermediate_hash[1] = 0xefcdab89;
 	context->intermediate_hash[2] = 0x98badcfe;
@@ -25,12 +24,11 @@ sha1_init(sha1_context_t *context)
 }
 
 void 
-sha1_input(uint8_t *message, uint64_t message_length, sha1_context_t *context) 
-{
+sha1_input(uint8_t *message, uint64_t message_length, sha1_context_t *context) {
 	int i;
 	context->message_length = message_length * 8;
 
-	for (i = 0; i < message_length; i++) {
+	for (i = 0; i < message_length; ++i) {
 		context->message_block[context->index] = *message++;
 		
 		if (context->index == 63) {
@@ -44,8 +42,7 @@ sha1_input(uint8_t *message, uint64_t message_length, sha1_context_t *context)
 } 
 
 void 
-sha1_process_block(sha1_context_t *context)
-{
+sha1_process_block(sha1_context_t *context) {
 	int i;
 	uint32_t message_words[80];
 
@@ -58,7 +55,7 @@ sha1_process_block(sha1_context_t *context)
 		 D = context->intermediate_hash[3], 
 		 E = context->intermediate_hash[4];
 		
-	for (i = 0; i < 20; i++) {
+	for (i = 0; i < 20; ++i) {
 		temp = A;
 		A = E + f1(B, C, D) + shift_left(A, 5) + message_words[i] + K1;
 		E = D;
@@ -67,7 +64,7 @@ sha1_process_block(sha1_context_t *context)
 		B = temp;
 	}
 	
-	for (i = 20; i < 40; i++) {	
+	for (i = 20; i < 40; ++i) {	
 		temp = A;
 		A = E + f2(B, C, D) + shift_left(A, 5) + message_words[i] + K2;
 		E = D;
@@ -76,7 +73,7 @@ sha1_process_block(sha1_context_t *context)
 		B = temp;
 	} 
 	
-	for (i = 40; i < 60; i++) {	
+	for (i = 40; i < 60; ++i) {	
 		temp = A;
 		A = E + f3(B, C, D) + shift_left(A, 5) + message_words[i] + K3;
 		E = D;
@@ -85,7 +82,7 @@ sha1_process_block(sha1_context_t *context)
 		B = temp;
 	} 
 	
-	for (i = 60; i < 80; i++) {	
+	for (i = 60; i < 80; ++i) {	
 		temp = A;
 		A = E + f2(B, C, D) + shift_left(A, 5) + message_words[i] + K4;
 		E = D;
@@ -104,25 +101,24 @@ sha1_process_block(sha1_context_t *context)
 }
 
 void 
-sha1_pad_message(sha1_context_t *context)
-{
+sha1_pad_message(sha1_context_t *context) {
 	int i, j;
 	
 	context->message_block[context->index++] = 0x80;
 
 	if (context->index > 56) {
-		for (i = context->index; i < 64; i++) {
+		for (i = context->index; i < 64; ++i) {
 			context->message_block[context->index++] = 0;
 		}
 		
 		sha1_process_block(context);
 	}
 
-	for (i = context->index; i < 56; i++) {
+	for (i = context->index; i < 56; ++i) {
 		context->message_block[context->index++] = 0;
 	}
 
-	for (j = 7; j >= 0; j--) {
+	for (j = 7; j >= 0; --j) {
 		context->message_block[context->index++] = context->message_length >> j * 8;
 	}
 
@@ -130,53 +126,47 @@ sha1_pad_message(sha1_context_t *context)
 }
 
 void 
-sha1_output(uint8_t *message_digest, sha1_context_t *context)
-{
+sha1_output(uint8_t *message_digest, sha1_context_t *context) {
 	int i;
-	for (i = 0; i < HASHSIZE; i++) {
+	for (i = 0; i < HASHSIZE; ++i) {
 		message_digest[i] = context->intermediate_hash[i/4] >> (3 - i % 4) * 8; 	
 	}
 }
 
 static uint32_t 
-shift_left(uint32_t val, const int x)
-{
+shift_left(uint32_t val, const int x) {
 	return val << x | val >> (32 - x);
 }
 
 static void 
-generate_words(uint8_t input_bytes[], uint32_t message_words[])
-{
+generate_words(uint8_t input_bytes[], uint32_t message_words[]) {
 	int i, shift_value;
 	
-	for (i = 0; i < 16; i++) {
+	for (i = 0; i < 16; ++i) {
 		message_words[i] = 0;
 	}
 
-	for (i = 0; i < 64; i++) {
+	for (i = 0; i < 64; ++i) {
 		shift_value = (uint32_t) input_bytes[i] << (24 - (i % 4) * 8); 
 		message_words[i/4] |= shift_value;
 	}
 	
-	for (i = 16; i < 80; i++) {
+	for (i = 16; i < 80; ++i) {
 		message_words[i] = shift_left(message_words[i - 16] ^ message_words[i - 14] ^ message_words[i - 8] ^ message_words[i - 3], 1);
 	}
 }
 
 static uint32_t 
-f1(uint32_t B, uint32_t C, uint32_t D) 
-{
+f1(uint32_t B, uint32_t C, uint32_t D) {
 	return (B & C) | (~B & D);
 }
 
 static uint32_t 
-f2 (uint32_t B, uint32_t C, uint32_t D) 
-{
+f2 (uint32_t B, uint32_t C, uint32_t D) {
 	return B ^ C ^ D;
 }
 
 static uint32_t 
-f3(uint32_t B, uint32_t C, uint32_t D) 
-{
+f3(uint32_t B, uint32_t C, uint32_t D) {
 	return (B & C) | (B & D) | (C & D);
 }
