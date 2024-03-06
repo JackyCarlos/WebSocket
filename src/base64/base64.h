df2c61d41e72b73d5b65b9ed8893eddf985818b8
Robert Eikmanns <Robert.Eikmanns@posteo.net>
2024-03-06 18:52:34 +0100

    change parameter types for input and output data respectively

--- src/base64/base64.h
+++ src/base64/base64.h
@@ -1,4 +1,4 @@
 #include <stdint.h>
 
-void base64_encode(const char *input_data, const uint32_t input_data_length, char *output_data, uint32_t *output_data_length);
-void base64_decode(const char *input_data, const uint32_t input_data_length, char *output_data, uint32_t *output_data_length);
+void base64_encode(const uint8_t *input_data, const uint32_t input_data_length, char *output_data, uint32_t *output_data_length);
+void base64_decode(const char *input_data, const uint32_t input_data_length, uint8_t *output_data, uint32_t *output_data_length);
