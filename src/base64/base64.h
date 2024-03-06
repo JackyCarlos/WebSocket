46e1fac3c00c502856719de3df718091356944ff
Robert Eikmanns <Robert.Eikmanns@posteo.net>
2024-03-06 17:00:34 +0100

    turn encode and decode functions into void functions

--- src/base64/base64.h
+++ src/base64/base64.h
@@ -1,4 +1,4 @@
 #include <stdint.h>
 
-char *base64_encode(const char *input_data, uint32_t input_data_length);
-char *base64_decode(const char *input_data, uint32_t input_data_length);
+void base64_encode(const char *input_data, const uint32_t input_data_length, char *output_data, uint32_t *output_data_length);
+void base64_decode(const char *input_data, const uint32_t input_data_length, char *output_data, uint32_t *output_data_length);
