eba7ea2ac2dfca8fe13a33186588d6c46a529a88
Robert Eikmanns <Robert.Eikmanns@posteo.net>
2024-03-07 12:04:33 +0100

    add comment for whole file

--- src/base64/base64.h
+++ src/base64/base64.h
@@ -1,3 +1,15 @@
+/***************************************************************************//**
+
+  @file         base64.h
+
+  @author       Robert Eikmanns
+
+  @date         Thursday, 7 March 2024
+
+  @brief        Declarations for base64 implementaion
+
+*******************************************************************************/
+
 #include <stdint.h>
 
 void base64_encode(const uint8_t *input_data, const uint32_t input_data_length, char *output_data, uint32_t *output_data_length);
