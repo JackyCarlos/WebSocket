53fff0d49564f332f52b9bbc023be3de5efabd0a
Robert Eikmanns <robert.eikmanns@htwk-leipzig.de>
2024-10-24 21:52:03 +0200

    add hashtags

--- src/debug/debug.h
+++ src/debug/debug.h
@@ -2,7 +2,7 @@
 #define DEBUG_H
 
 #ifdef DEBUG_MODE
-    #define DEBUG_PRINT(fmt, ...) printf(fmt, __VA_ARGS__)
+    #define DEBUG_PRINT(fmt, ...) printf(fmt, ##__VA_ARGS__)
 #else
     #define DEBUG_PRINT(fmt, ...) // do nothing
 #endif
